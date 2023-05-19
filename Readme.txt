90d6e2e01a02d10104db4e5e324fb1efa11315b8
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-05-19 03:03:14 +0100

    Project start

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+The Angular-based events management web app is designed for learning purposes,
+providing an intuitive platform for organizing and managing various events.
+Users can effortlessly create, schedule, and promote events,
+while attendees can easily browse through event listings, register,
+and receive updates.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './Events/event-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule
