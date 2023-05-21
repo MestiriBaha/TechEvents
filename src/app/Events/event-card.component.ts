3a77ed96570dc5360f67a13e1ab43d80c42e0ef7
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-05-22 00:23:25 +0100

    creating events elements | importing  UI elements

---
+++ src/app/Events/event-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { events } from '../Data/Events';
+
+
+@Component({
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.css']
+})
+export class EventCardComponent {
+   staticData = events ; 
+
+
+}
--- src/app/Events/event-list.component.ts
+++ src/app/Events/event-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
